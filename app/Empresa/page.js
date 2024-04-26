cd77dcc8497ea593430b1378231816e1e91103ae
07santiago21 <santiagolopezramirez0721@gmail.com>
2024-04-25 23:47:27 -0500

    page Empresas and page Personas init-version

--- app/Empresa/page.js
+++ app/Empresa/page.js
@@ -1,32 +1,170 @@
 export default function Example() {
   return (
     <>
-      {/*
-        This example requires updating your template:
+    
+    <main className=" bg-[#D3FB7F]">
 
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">!!!</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">En construccion</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">pronto estara disponible la pagina Empresa.</p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="/"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              regresar
-            </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
-              Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </div>
-      </main>
+    <div className="flex justify-center items-center h-screen"
+    style={{background:"#D3FB7F" }}
+    >
+      {/* Lado izquierdo: Imagen */}
+      <div className="w-1/2 h-full flex justify-center items-center relative"
+        
+      
+      >
+
+        <h2 className="absolute top-0 mt-10 text-center text-4xl font-bold leading-9 tracking-tight" style={{marginTop:"5%" , color:'#df4869'}}>
+          Soy Empresa</h2>
+        <img src="https://neggo.my.canva.site/images/9f9e562e2ea842f7132cd0ef59605967.svg" alt="Image" className="max-w-full max-h-full" />
+      </div>
+
+      {/* Lado derecho: Formulario */}
+      <div className="w-1/2 h-full flex justify-center items-center"
+     
+      >
+        
+        <div className="flex min-h-full flex-1 flex-col justify-center px-2 py-5 lg:px-8 ml-auto"
+
+        style={{background:"white", marginRight: '20%', marginBottom: '5%' }}
+      >
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight" style={{ color: '#df4869',marginTop:"0%" }}>
+                ¡Seras el primero o la primera en saber!
+                </h2>
+              </div>
+
+              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                  <div >
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    NOMBRE DE LA EMPRESA
+
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="company_name"
+                        name="company_name"
+                        type="company_name"
+                        autoComplete="company_name"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}  
+                      
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      NIT DE LA EMPRESA
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="nit"
+                        name="nit"
+                        type="nit"
+                        autoComplete="nit"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+                  </div>
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      NOMBRE DEL CONTACTO
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="name"
+                        name="name"
+                        type="name"
+                        autoComplete="name"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+                  </div>
+
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      CELULAR DEL CONTACTO
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="cel"
+                        name="cel"
+                        type="cel"
+                        autoComplete="cel"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+                  </div>
+
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900" >
+                      CORREO DE LA EMPRESA
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+                  </div>
+
+
+                  <div  className="text-center space-y-4">
+                    <button
+                      type="submit"
+                      inline-block rounded-full px-8 py-3 text-center font-medium  style={{background:"#df4869", color:"white" }}
+                      className="flex w-1/2 mx-auto justify-center rounded-full px-8 py-3 text-center font-medium"
+
+
+                    >
+                      Enviar
+                    </button>
+                    <p  style={{color:"#df4869"}}>¡pronto nos comunicaremos contigo!</p>
+
+                  </div>
+                </form>
+
+                
+              </div>
+
+      </div>
+      
+
+      </div>
+    </div>
+
+
+    
+
+    </main>      
     </>
   )
 }
--- app/Persona/page.js
+++ app/Persona/page.js
@@ -1,32 +1,177 @@
 export default function Example() {
   return (
     <>
-      {/*
-        This example requires updating your template:
+    
+    <main className=" bg-[#D3FB7F]">
 
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">!!!</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">En construccion</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">pronto estara disponible la pagina Persona.</p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="/"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              regresar
-            </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
-              Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </div>
-      </main>
+    <div className="flex justify-center items-center h-screen"
+    style={{background:"#D3FB7F" }}
+    >
+      {/* Lado izquierdo: Imagen */}
+      <div className="w-1/2 h-full flex justify-center items-center relative"
+        
+      
+      >
+
+        <h2 className="absolute top-0 mt-10 text-center text-4xl font-bold leading-9 tracking-tight" style={{marginTop:"5%" , color:'#df4869'}}>
+          Soy persona</h2>
+        <img src="https://neggo.my.canva.site/images/9f9e562e2ea842f7132cd0ef59605967.svg" alt="Image" className="max-w-full max-h-full" />
+      </div>
+
+      {/* Lado derecho: Formulario */}
+      <div className="w-1/2 h-full flex justify-center items-center"
+     
+      >
+        
+        <div className="flex min-h-full flex-1 flex-col justify-center px-2 py-5 lg:px-8 ml-auto"
+
+        style={{background:"white", marginRight: '20%', marginBottom: '5%' }}
+      >
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight" style={{ color: '#df4869',marginTop:"0%" }}>
+                ¡Seras el primero o la primera en saber!
+                </h2>
+              </div>
+
+              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                  <div >
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    NOMBRE
+
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="name_user"
+                        name="name_user"
+                        type="name_user"
+                        autoComplete="name_user"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}  
+                      
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      APELLIDO
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="last_name_user"
+                        name="last_name_user"
+                        type="last_name_user"
+                        autoComplete="last_name_user"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+                  </div>
+
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      CEDULA
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="id_user"
+                        name="id_user"
+                        type="id_user"
+                        autoComplete="id_user"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+                  </div>
+
+
+
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      CELULAR DEL CONTACTO
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="cel_user"
+                        name="cel_user"
+                        type="cel_user"
+                        autoComplete="cel_user"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+
+                  
+                  </div>
+
+
+                
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      CORREO PERSONAL
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="email_user"
+                        name="email_user"
+                        type="email_user"
+                        autoComplete="email_user"
+                        required
+                        className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        style={{background:"#ececec" }}
+                      />
+                    </div>
+
+                  </div>
+
+
+                  <div  className="text-center space-y-4">
+                    <button
+                      type="submit"
+                      inline-block rounded-full px-8 py-3 text-center font-medium  style={{background:"#df4869", color:"white" }}
+                      className="flex w-1/2 mx-auto justify-center rounded-full px-8 py-3 text-center font-medium"
+
+
+                    >
+                      Enviar
+                    </button>
+                    <p  style={{color:"#df4869"}}>¡pronto nos comunicaremos contigo!</p>
+
+                  </div>
+                </form>
+
+                
+              </div>
+
+      </div>
+      
+
+      </div>
+    </div>
+
+
+    
+
+    </main>      
     </>
   )
 }
